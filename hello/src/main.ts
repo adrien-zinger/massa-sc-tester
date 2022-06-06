d2e01bd061b444ef2c920b3029d0af00e432030d
Adrien Zinger <zinger.ad@gmail.com>
2022-06-06 13:24:26 +0200

    add stuff to test mem leaks

---
+++ hello/src/main.ts
@@ -0,0 +1,13 @@
+/** ****************************
+ * Bytecode to send to the massa network that push the `helloworld`
+ * smartcontract.
+ *
+ * N.B. The entry file of your Massa Smart Contract program should be named
+ * `src/main.ts`, the command `yarn bundle` will produce an `build/main.wasm`
+ * which is ready to be send on Massa network node!
+ **/
+
+
+export function main(_args: string): i32 {
+	return 12;
+}
